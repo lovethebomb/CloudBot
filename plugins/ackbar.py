a60cc82ade062d2347e681e8229edf0e016fcc05
Lucas <lucas@lovethebomb.eu>
2013-08-13 17:13:14 +0200

    Add misc. and useless plugins

--- plugins/ackbar.py
+++ plugins/ackbar.py
@@ -11,19 +11,16 @@ from random import choice
 
 @hook.singlethread
 @hook.event('PRIVMSG')
-def ackbar(paraml, input=None, say=None):
-    ackbars = [
-        "http://dayofthejedi.com/wp-content/uploads/2011/03/171.jpg",
-        "http://dayofthejedi.com/wp-content/uploads/2011/03/152.jpg",
-        "http://farm4.static.flickr.com/3572/3637082894_e23313f6fb_o.jpg",
-        "http://www.youtube.com/watch?v=dddAi8FF3F4",
-        "http://6.asset.soup.io/asset/0610/8774_242b_500.jpeg",
-        "http://files.g4tv.com/ImageDb3/279875_S/steampunk-ackbar.jpg",
-        "http://farm6.staticflickr.com/5126/5725607070_b80e61b4b3_z.jpg",
-        "http://farm6.static.flickr.com/5291/5542027315_ba79daabfb.jpg",
-        "http://farm5.staticflickr.com/4074/4751546688_5c76b0e308_z.jpg",
-        "http://farm6.staticflickr.com/5250/5216539895_09f963f448_z.jpg"
+def nein(paraml, input=None, say=None, nick=None):
+    nein = [
+        "http://www.youtube.com/watch?feature=player_detailpage&v=1MLry6Cn_D4",
+        "http://www.youtube.com/watch?v=kQeKskCvJwc",
+        "http://www.youtube.com/watch?feature=player_detailpage&v=h4se9_vexUM",
+        "http://www.youtube.com/watch?v=8n4_BxQlCFg",
+        "http://www.youtube.com/watch?v=bj2KSiEeCcM",
+        "http://www.youtube.com/watch?v=ILtxKWHrGyQ",
+        "http://www.youtube.com/watch?v=sLs19nIikwQ",
     ]
 
-    if match("^it'?s a trap", input.msg.lower()):
-        say("%s" % choice(ackbars))
+    if match(".* nein", input.msg.lower()):
+        say("Ach, %s !  %s" % (nick, choice(nein)))
