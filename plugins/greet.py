8415da14decf157b143c140abec617890abf1e2d
root <root@ks384455.kimsufi.com>
2013-11-22 11:20:03 +0100

    Fix 'NoneType' object is not callable, removed say arg and return directly the answer

--- plugins/greet.py
+++ plugins/greet.py
@@ -1,25 +1,29 @@
 import re
-from util import hook
+from util   import hook
 from random import choice
 
 
 hello_re = (r'(^(hello|hi|bonjour|salut|hey|plop|pouet|yo).?.?$)', re.I)
-back_re = (r'(^(back|re|me revoila|bk).?$)', re.I)
-shake_re = (r'(^(\\o/|\\o\\|/o/|\|\\o/\|).?$)', re.I)
+back_re  = (r'(^(back|re|me revoila|bk).?$)',                      re.I)
+shake_re = (r'(^(\\o/|\\o\\|/o/|\|\\o/\|).?$)',                    re.I)
 
 @hook.regex(*hello_re)
-def greet(match, nick=None, say=None):
+def greet(match, nick = None):
     greets = ["Hello", "Pouet", "plop", "Salut", "Yo", "Wassup", "Bonjour", "Hallo", "Hola", "Bonjourno", "Moin moin"]
-    say("%s %s!" % (choice(greets), nick))
+    
+    return '{greet!s} {nick!s}!'.format(nick = nick, greet = choice(greets))
 
 
 @hook.regex(*back_re)
-def welcomeback(match, nick=None, say=None):
-    greets = ["Welcome back", "re", "re-bienvenu", "wb"]
-    say("%s %s!" % (choice(greets), nick))
+def welcomeback(match, nick = None):
+    greets = ["Welcome back", "re",  "re-bienvenu", "wb"]
+    
+    return '{greet!s} {nick!s}!'.format(nick = nick, greet = choice(greets))
 
 
 @hook.regex(*shake_re)
-def shake(match, say=None):
+def shake(match):
     greets = ["\o/", "/o/", "\o\\", "|\o/|"]
-    say("%s" % choice(greets))
+    
+    return '{greet!s}'.format(greet = choice(greets))
+	
