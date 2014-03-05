f695fcc9596b8fcafd7f266f48bcda23857a93d1
jackismissing <nicolas.j.garnier@gmail.com>
2014-03-05 14:06:40 +0100

    Added support for fr wikipedia

--- plugins/wikipedia.py
+++ plugins/wikipedia.py
@@ -8,30 +8,37 @@ from util import hook, http, text
 
 api_prefix = "http://en.wikipedia.org/w/api.php"
 search_url = api_prefix + "?action=opensearch&format=xml"
+search_url_fr = api_prefix + search_url + "&prop=langlinks&lllang=fr"
 
 paren_re = re.compile('\s*\(.*\)$')
 
 
 @hook.command('w')
 @hook.command
-def wiki(inp):
+def wiki_en(inp):
+    get_items(inp, search_url)
+
+@hook.command('wfr')
+@hook.command
+def wiki_fr(inp):
+    get_items(inp, search_url_fr)
+
+def get_items(inp, url_query):
     """wiki <phrase> -- Gets first sentence of Wikipedia article on <phrase>."""
-
-    x = http.get_xml(search_url, search=inp)
-
+    x = http.get_xml(url_query, search=inp)
     ns = '{http://opensearch.org/searchsuggest2}'
     items = x.findall(ns + 'Section/' + ns + 'Item')
-
+    
     if not items:
         if x.find('error') is not None:
             return 'error: %(code)s: %(info)s' % x.find('error').attrib
         else:
-            return 'No results found.'
+            return 'Le results not found'
 
     def extract(item):
         return [item.find(ns + x).text for x in
                             ('Text', 'Description', 'Url')]
-
+    
     title, desc, url = extract(items[0])
 
     if 'may refer to' in desc:
@@ -43,7 +50,7 @@ def wiki(inp):
         desc = title + desc
 
     desc = re.sub('\s+', ' ', desc).strip()  # remove excess spaces
-
     desc = text.truncate_str(desc, 200)
 
     return '{} :: {}'.format(desc, http.quote(url, ':/'))
+
